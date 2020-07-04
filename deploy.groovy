31d9f761a5bca294216403b41d86324f00c4037b
Tanya Chetna Vaish <63603952+TanyaChetnaVaish@users.noreply.github.com>
2020-07-04 20:46:01 +0530

    Create deploy.groovy

---
+++ deploy.groovy
@@ -0,0 +1,82 @@
+job("kube1_groovy"){
+  description("kubernetes job1")
+  scm{
+    github('TanyaChetnaVaish/devopstask6a','master')
+  }
+  steps{
+    shell('sudo cp -vrf * /task6/jenkins')
+  }
+  triggers{
+    gitHubPushTrigger()
+  }
+}
+
+
+job("kube2_groovy"){
+  steps{
+    shell('''
+	if sudo ls /task6/jenkins | grep php
+      	then
+		if sudo kubectl get deployment --selector "app in (httpd)" | grep httpd-web
+    		then
+			sudo kubectl apply -f /task6/jenkins/deploy.yml
+           		POD=$(sudo kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
+        		echo $POD
+        		sudo kubectl cp /task6/jenkins/index.php $POD:/var/www/html
+		else
+    			if ! kubectl get pvc | grep httpdweb1-pv-claim
+        		then
+            			sudo kubectl create -f /task6/jenkins/pvc.yml
+        		fi
+        		sudo kubectl create -f /task6/jenkins/deploy.yml
+        		POD=$(sudo kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
+        		echo $POD
+        		sudo kubectl cp /task6/jenkins/index.php $POD:/var/www/html
+    		fi
+   	fi
+	''')
+  }
+  triggers {
+        upstream('kube1_groovy', 'SUCCESS')
+  }
+}
+
+job("kube3_groovy")
+{
+  steps{
+    shell('''
+status=$(curl -o /dev/null -s -w "%{http_code}" http://192.168.99.104:30002)
+if [[ $status == 200 ]]
+then
+    echo "Running"
+    exit 0
+else
+     exit 1
+fi
+     ''')
+  }
+  
+  triggers {
+        upstream('kube2_groovy', 'SUCCESS')
+  }
+  
+  publishers {
+        extendedEmail {
+            recipientList('tanya.vaish.743@gmail.com')
+            defaultSubject('Job status')
+          	attachBuildLog(attachBuildLog = true)
+            defaultContent('Status Report')
+            contentType('text/html')
+            triggers {
+                always {
+                    subject('build Status')
+                    content('Body')
+                    sendTo {
+                        developers()
+                        recipientList()
+                    }
+		}
+	    }
+	}
+    }
+}
